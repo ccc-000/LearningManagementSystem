194622f2606aa0fd6c17c93e13617170296ee1a3
Zehui-He <92138586+Zehui-He@users.noreply.github.com>
2023-03-12 01:20:42 +1100

    added loginpage and gitignore for python

--- hydra_frontend/src/App.js
+++ hydra_frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
 
@@ -11,24 +11,11 @@ function App() {
 
   connect();
 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hydra</h1>
+      <Button type="primary">Primary Button</Button>
+    </>
   );
 }
 
---
+++ hydra_frontend/src/pages/Loginpage.js
@@ -0,0 +1,109 @@
+import '../styles/loginpage.css';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+export default function Loginpage() {
+
+    const onFinish = (values) => {
+        if (values.username === 'admin' && values.password === 'admin') {
+            window.location.href = '/dashboard';
+        }
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    function connect() {
+        fetch('http://localhost:8000', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify({
+                username: 'admin',
+                password: 'admin',
+            }),
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    connect();
+
+    return (
+        <>
+            <div className='loginpage'>
+            <div className="logincard">
+                <h1 className='welcomemsg'>Welcome to the Hydra learning management system!</h1>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            </div>
+            </div>
+        </>
+    );
+}
