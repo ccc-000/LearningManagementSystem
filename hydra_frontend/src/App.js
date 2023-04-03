e77e915c045a1ba74bc599af30b97d26af924219
Gemma <GemmaLiu0922@gmail.com>
2023-04-04 03:35:16 +1000

    Upload 半成品 forum detail(student) Page

--- hydra_frontend/src/App.js
+++ hydra_frontend/src/App.js
@@ -15,6 +15,7 @@ import Coursemainpage from "./pages/Coursemainpage";
 import Forum from "./pages/Forum";
 import CreateForum from './pages/CreateForum';
 import EditForum from './pages/EditForum';
+import ForumDetailStudent from './pages/ForumDetail-student';
 import * as React from 'react'
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
                 <Route path="/forum" element={<Forum/>}/>
                 <Route path="/createforum" element={<CreateForum/>}/>
                 <Route path="/editforum" element={<EditForum/>}/>
+                <Route path="/forumdetailstudent" element={<ForumDetailStudent/>}/>
             </Routes>
         </>
     );
