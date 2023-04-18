280c7a8e1c379a42962403b3c51e9941a302aae2
Jingyao Chen <z5383013@ad.unsw.edu.au>
2023-04-18 17:31:42 +1000

    course main page done

--- hydra_frontend/src/pages/Coursemainpage.js
+++ hydra_frontend/src/pages/Coursemainpage.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Button, Layout, theme, Tooltip } from 'antd';
+import { Button, Layout, theme, Tooltip, Card } from 'antd';
 import Navibar from '../components/Navibar';
 import { RollbackOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const role = localStorage.getItem('role');
-console.log(role);
 
 const Coursemainpage = () => {
+  const role = localStorage.getItem('role');
+  const cname = localStorage.getItem('cname');
+  const cdes = localStorage.getItem('cdes');
+
+
   const {
       token: { colorBgContainer },
   } = theme.useToken();
@@ -21,14 +25,18 @@ const Coursemainpage = () => {
           minHeight: '100vh',
           marginLeft: 200,
       }}>
-      {/* <Header courseName={getCourseName()} style={{padding:'5px 10px'}} > */}
         <Header style={{ padding: '2px 10px' }}>
           <Link to='/dashboardLecturer'>
             <Tooltip title="Back">
               <Button type='link' shape="circle" icon={<RollbackOutlined />} />
             </Tooltip>
           </Link>
+          <h2 style={{display: 'inline-block', marginLeft: '20px', color:'white'}}>{cname}</h2>
         </Header>
+        <Card title="Course Description" bordered={false} style={{ width:'90%', marginTop: 25, marginLeft:25 }}>
+          <p>{cdes}</p>
+        </Card>
+
         <Navibar />   
         <Footer
         style={{
@@ -46,14 +54,14 @@ const Coursemainpage = () => {
           minHeight: '100vh',
           marginLeft: 200,
       }}>
-      {/* <Header courseName={getCourseName()} style={{padding:'5px 10px'}} > */}
-        <Header style={{ padding: '2px 10px' }}>
-          <Link to='/dashboard'>
-            <Tooltip title="Back">
-              <Button type='link' shape="circle" icon={<RollbackOutlined />} />
-            </Tooltip>
-          </Link>
-        </Header>
+      <Header style={{ padding: '2px 10px' }}>
+        <Link to='/dashboardLecturer'>
+          <Tooltip title="Back">
+            <Button type='link' shape="circle" icon={<RollbackOutlined />} />
+          </Tooltip>
+        </Link>
+        <h2 style={{display: 'inline-block', marginLeft: '20px', color:'white'}}>{cname}</h2>
+      </Header>
         <Navibar />   
         <Footer
         style={{
